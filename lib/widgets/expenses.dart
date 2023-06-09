c03bb61fde82394baf729a9e8ee12f8142e54013
Kelvin Almeida <58817347+kelvinalmeida@users.noreply.github.com>
2023-06-09 11:18:21 -0300

    adding content in New_Expense

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widgets/expenses/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -35,7 +36,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAllExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => const Text('Modal botton Sheet'),
+      builder: (ctx) => const NewExpense(),
     );
   }
 
